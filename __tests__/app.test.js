3d5598bcc4cba5d88219b62677e50a191504d99e
naydenhristov <nhristov21@gmail.com>
2025-04-28 12:48:36 +0100

    this commit sets up the middleware, controler and model, also solves task 1

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,15 +1,26 @@
 const endpointsJson = require("../endpoints.json");
-/* Set up your test imports here */
+const request = require("supertest");
+const db = require("../db/connection");
+const app = require("../api.js");
+const seed = require("../db/seeds/seed");
+const data = require("../db/data/test-data");
 
 /* Set up your beforeEach & afterAll functions here */
+beforeEach(() => {
+  return seed(data);
+});
+
+afterAll(() => {
+  return db.end();
+});
 
 describe("GET /api", () => {
-  test.skip("200: Responds with an object detailing the documentation for each endpoint", () => {
+  test.only("200: Responds with an object detailing the documentation for each endpoint", () => {
     return request(app)
       .get("/api")
       .expect(200)
-      .then(({ body: { endpoints } }) => {
-        expect(endpoints).toEqual(endpointsJson);
+      .then((response) => {
+        expect(response.body).toEqual(endpointsJson);
       });
   });
 });
