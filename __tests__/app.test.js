6360d77b7ac505ae790c1c12f0b945577558dc4d
naydenhristov <nhristov21@gmail.com>
2025-04-29 08:59:11 +0100

    this commit will solve task 3 - GET api/articles/:article_id

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -30,8 +30,8 @@ describe("GET /api/topics", () => {
     return request(app)
       .get("/api/topics")
       .expect(200)
-      .then((response) => {
-        const topics = response.body.topics;
+      .then(({body}) => {
+        const topics = body.topics;
         expect(topics).toHaveLength(3);
         expect(typeof topics).toBe("object");
         topics.forEach((topic) => {
@@ -48,8 +48,47 @@ describe("GET /api/topics", () => {
 describe("GET /wrongpath", () => {
   test("404: not found, spelling mistake in the path / wrong path", () => {
     return request(app).get("/wrongpath").expect(404)
-    .then((response) => {
-      expect(response.body.msg).toBe("Not Found");
+    .then(({body}) => {
+      expect(body.msg).toBe("Not Found");
     }); 
+  });
+});
+
+describe("GET /api/articles/:article_id", () => {
+  test("200: sends response with correct article_id", () => {
+    return request(app)
+      .get("/api/articles/1")
+      .expect(200)
+      .then(({ body }) => {
+        expect(body.article).toEqual({
+          article_id: 1,
+          title: "Living in the shadow of a great man",
+          topic: "mitch",
+          author: "butter_bridge",
+          body: "I find this existence challenging",
+          created_at: "2020-07-09T20:11:00.000Z",
+          votes: 100,
+          article_img_url:
+            "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
+        });
+      });
+  });
+
+  test("400: Bad Request! when a string passed instead of valid article_id", () => {
+    return request(app)
+      .get("/api/articles/NotAnId")
+      .expect(400)
+      .then(({body}) => {
+        expect(body.msg).toBe("Bad Request!");
+      })
+  });
+
+  test("404: Not Found! when passed a valid number, but not existing article_id", () => {
+    return request(app)
+      .get("/api/articles/1000")
+      .expect(404)
+      .then(({body}) => {
+        expect(body.msg).toBe("Not Found!");
+      })
   });
 });
--- app/nc_news.model.js
+++ app/nc_news.model.js
@@ -1,8 +1,17 @@
 const db = require("../db/connection");
 
 exports.selectTopics = () => {
-    return db.query("SELECT * FROM topics").then((result) =>{
-   
-      return result.rows;
-    })
-}
+  return db.query("SELECT * FROM topics").then((result) => {
+    return result.rows;
+  });
+};
+
+exports.selectArticleById = (article_id) => {
+    return db.query(`SELECT * FROM articles WHERE article_id = $1`, [article_id]).then((result) => {
+          if(!result.rows.length) {
+              return Promise.reject({ status: 404, msg: "Not Found!"})
+          }
+  
+        return result.rows[0];
+      });
+};
