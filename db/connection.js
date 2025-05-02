b22c8f203729cca6d7124c60b1095bae8652a956
naydenhristov <nhristov21@gmail.com>
2025-05-02 09:47:58 +0100

    small changes in connection.js

--- db/connection.js
+++ db/connection.js
@@ -1,8 +1,8 @@
 const { Pool } = require("pg");
 
-const ENV = process.env.NODE_ENV || 'development'
+const ENV = process.env.NODE_ENV || 'development';
 
-require('dotenv').config({path: `${__dirname}/../.env.${ENV}`})
+require('dotenv').config({path: `${__dirname}/../.env.${ENV}`});
 
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
@@ -17,5 +17,6 @@ if (ENV === "production") {
   config.connectionString = process.env.DATABASE_URL;
   config.max = 2;
 }
+
 module.exports = new Pool(config);
 
